0b23748a5e8df06602843adffbff2b77d290739e
Dinh Phuc Tuan Nhat <nhat18349276@gmail.com>
2025-04-04 01:27:28 +0700

    done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,16 @@ plugins {
 
 android {
     namespace = "com.example.tiptime"
-    compileSdk = 34
+    compileSdk = 35
 
+
+    buildFeatures {
+        viewBinding = true
+    }
     defaultConfig {
         applicationId = "com.example.tiptime"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -5,16 +5,35 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
+import kotlin.math.ceil
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.calculateButton.setOnClickListener{ calculateTip() }
+    }
+    fun calculateTip(){
+        val costString = binding.cost.text.toString()
+        val cost = costString.toDouble();
+        val selectedId = binding.tipOption.checkedRadioButtonId;
+        val tipPercentage = when(selectedId){
+            R.id.p20 -> 0.2
+            R.id.p18 -> 0.18
+            else -> 0.15
         }
+        var tip = cost * tipPercentage;
+        var roundUp = binding.roundSwitch.isChecked;
+        if(roundUp){
+            tip = ceil(tip);
+        }
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip);
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
     }
 }
